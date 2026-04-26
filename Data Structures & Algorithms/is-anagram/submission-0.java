73018874bf4a13bde5fa3763017b19ea5c86526d
qml219 <118629704+qml219@users.noreply.github.com>
2026-04-26 15:09:40 -0400

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> h = new HashMap<Character, Integer>();
+        int m = s.length();
+        int n = t.length();
+        if (m != n) return false;
+        for (int i = 0; i < m; i++) {
+            char curr = s.charAt(i);
+            if (h.containsKey(curr)) {
+                h.put(curr, h.get(curr) + 1);
+            }
+            else {
+                h.put(curr, 1);
+            }
+        }
+        
+        for (int j = 0; j < n; j++) {
+            char curr = t.charAt(j);
+            if (h.containsKey(curr)) {
+                int curr_freq = h.get(curr);
+                if (curr_freq == 0) return false;
+                if (curr_freq == 1) h.remove(curr);
+                h.put(curr, curr_freq - 1);
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
